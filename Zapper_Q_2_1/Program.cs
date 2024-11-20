583d5d913ca463b1a98d5165c54890e26d6e11c4
Malcolm Carelse <malcolmcarelse@gmail.com>
2024-11-20 17:21:09 +0200

    Added Question 2.1 and 2.2

---
+++ Zapper_Q_2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿bool CheckSettings(string settings, int setting)
+{
+    if (settings[setting - 1].Equals('1'))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+Console.WriteLine("Question 2.1");
+Console.WriteLine(CheckSettings("00000000", 7));
+Console.WriteLine(CheckSettings("00000010", 7));
+Console.WriteLine(CheckSettings("11111111", 4));
---
+++ Zapper_Q_2_2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Question 2.2");
+void WriteToFile(string setting)
+{
+    using (StreamWriter output = new StreamWriter("settings.txt", true))
+    {
+        output.Write(setting + ",");
+    }
+}
+
+string[] ReadFromFile()
+{
+    StreamReader reader = new StreamReader("settings.txt");
+
+    return reader.ReadToEnd().Split(",", StringSplitOptions.RemoveEmptyEntries);
+}
+
+WriteToFile("00000000");
+WriteToFile("00000010");
+WriteToFile("11111111");
+
+string[] settings = ReadFromFile();
+
+foreach (var item in settings)
+{
+    Console.WriteLine(item);
+}
